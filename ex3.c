21400395361ca25df679ff9caa8d99b7b02fecde
Ghaadyy <ghadyyi@gmail.com>
2024-10-06 20:22:32 +0300

    Exercise 3 formatted

--- ex3.c
+++ ex3.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef union font {
-    struct info {
+typedef union font
+{
+    struct info
+    {
         uint8_t bold : 1;
         uint8_t italic : 1;
         uint8_t size : 6;
@@ -11,8 +13,9 @@ typedef union font {
 
     uint16_t font;
 } font;
- 
-void get_font_info(union font *f) {
+
+void get_font_info(union font *f)
+{
     printf("The font extracted contains the folliwng properties:\n");
     printf("Bold: %i\n", f->info.bold);
     printf("Italic: %i\n", f->info.italic);
@@ -20,7 +23,8 @@ void get_font_info(union font *f) {
     printf("Type: %i\n", f->info.type);
 }
 
-int main() {
+int main()
+{
     union font f;
     f.font = 0xFFFF;
 
