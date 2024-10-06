3cf17a14a1ef52cdd2d9c51f9889069498940f87
Ghaadyy <ghadyyi@gmail.com>
2024-10-06 21:31:28 +0300

    Fixed exercise 4

--- ex4/Firmware/src/main.c
+++ ex4/Firmware/src/main.c
@@ -1,29 +1,67 @@
 #include "main.h"
 #include "hardware.h"
 #include "system.h"
+#include <stdint.h>
+
+bool toggle = false;      // Toggle delay from 1s to 2s (and vice-versa)
+uint16_t B0_DELAY = 500;  // Delay for PIN_B0
+uint16_t b1_delay = 1000; // Delay for PIN_B1
+
+void toggle_delay()
+{
+    if (toggle)
+    {
+        b1_delay = 1000;
+    }
+    else
+    {
+        b1_delay = 2000;
+    }
+    toggle = !toggle;
+}
 
 void main()
 {
-    init_hw();                          /*Initialize HW (IOs ports)*/
+    init_hw();     // Initialize HW (IO ports)
+    delay_ms(100); // Power up delay, wait for 100ms
 
-    delay_ms(100);                      /*power up delay, wait a 100ms*/
-
-	while(1)                            /*main infinite loop*/
+    while (1) // Infinite loop
     {
-        if (!input(PIN_B0))             /*PB1 pressed?*/
+        if (!input(PIN_B0)) // Handle case when D1 and D2 are flashing
         {
-            output_high(PIN_C0);
+            while (!input(PIN_B0))
+            {
+                /*
+                    Distribute output in 500ms periods to
+                    incrementally light D1 depending on D2's delay
+                 */
+                for (uint8_t i = 0; i < b1_delay / B0_DELAY; i++)
+                {
+                    output_toggle(PIN_C0);
+                    delay_ms(500);
+                }
+
+                output_toggle(PIN_C1);
+            }
         }
-        else                            /*PB1 released?*/
+        else // D2 is only flashing
         {
             output_low(PIN_C0);
-        }
+            if (input(PIN_B1))
+            {
+                while (input(PIN_B1))
+                {
+                    output_toggle(PIN_C1);
+                    delay_ms(b1_delay);
+                }
 
-        if (input(PIN_B1))              /*PB2 pressed?*/
-        {
-            while(input(PIN_B1))    {}  /*while still pressed, just wait...*/
-            output_toggle(PIN_C1);      /*Do it!*/
+                toggle_delay();
+            }
+            else
+            {
+                output_toggle(PIN_C1);
+                delay_ms(b1_delay);
+            }
         }
-        
     }
 }
